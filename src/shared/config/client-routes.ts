6d1a66498c58c7af9acd2399ba605e20ed7ef629
kisszaya <sveta.matvik.sveta@gmail.com>
2022-10-03 23:25:53 +0300

    feat(shared/config): add files api.ts and client-routes.ts

---
+++ src/shared/config/client-routes.ts
@@ -0,0 +1,12 @@
+export enum PUBLIC_PATH {
+  LOGIN = "/login",
+  SIGNUP = "/signup",
+}
+
+export enum USER_PATH {
+  SPACES = "/spaces",
+}
+
+export enum ADMIN_PATH {
+  REPORTS = "/reports",
+}
